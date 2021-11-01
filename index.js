bdbc4a4b9941acb840375389d3ace7ee43fedac9
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-10-31 21:26:29 -0600

    Added getAPI function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("A book repository for you!")
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
