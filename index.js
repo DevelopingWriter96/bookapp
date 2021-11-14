d25c880f513f0f7fcd5f113ded18f76d8662384a
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-11-14 16:23:46 -0700

    re-added api until database can be attached

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("A book repository for you!")
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getAPIData("https://openlibrary.org/works/OL45883W.json")
