f340ed4d275346baaec2e547b04626a0a97fdc1d
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-11-14 19:50:17 -0700

    Added env and server

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {mongoPass, mongoUser} = require('./config')
+
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@learning.bm32j.mongodb.net/Learning?retryWrites=true&w=majority` 
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected Successfully')
+})
+.catch(console.error)
